be09a2695d140a5d1f10390bcbae2ebda826ef7a
Brendon Otto <brendon@brendonotto.com>
2018-05-23 22:58:36 -0500

    Committing to source, finished tutorial

---
+++ lib/nerves_rpi3_wifi_picam.ex
@@ -0,0 +1,22 @@
+defmodule NervesRpi3WifiPicam do
+  @moduledoc """
+  Documentation for NervesRpi3WifiPicam.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesRpi3WifiPicam.hello
+      :world
+
+  """
+  def take_and_read_picture() do
+    Picam.Camera.start_link
+
+    Picam.next_frame
+    |> Base.encode64()
+    |> IO.puts()
+  end
+end
---
+++ lib/streamer.ex
@@ -0,0 +1,38 @@
+defmodule NervesRpi3WifiPicam.Streamer do
+    @moduledoc """
+    Plug for streaming an image
+    """
+    import Plug.Conn
+
+    @behaviour Plug
+    @boundary "w58EW1cEpjzydSCq"
+
+    def init(opts), do: opts
+
+    def call(conn, _opts) do
+        conn
+        |> put_resp_header("Age", "0")
+        |> put_resp_header("Cache-Control", "no-cache, private")
+        |> put_resp_header("Pragma", "no-cache")
+        |> put_resp_header("Content-Type", "multipart/x-mixed-replace; boundary=#{@boundary}")
+        |> send_chunked(200)
+        |> send_pictures
+    end
+
+    defp send_pictures(conn) do
+        send_picture(conn)
+        send_pictures(conn)
+    end
+
+    defp send_picture(conn) do
+        Picam.set_size(1280, 0)
+        jpg = Picam.next_frame
+        size = byte_size(jpg)
+        header = "------#{@boundary}\r\nContent-Type: image/jpg\r\nContent-Length: #{size}\r\n\r\n"
+        footer = "\r\n"
+        with {:ok, conn} <- chunk(conn, header),
+             {:ok, conn} <- chunk(conn, jpg),
+             {:ok, conn} <- chunk(conn, footer),
+            do: conn
+    end
+end
